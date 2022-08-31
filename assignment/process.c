89719c0289f09b1e7a6d400f51294d831969a8b0
sre90 <sre90@uclive.ac.nz>
2022-08-31 16:53:07 +1200

    improved safety by adding signal handler

--- assignment/process.c
+++ assignment/process.c
@@ -14,16 +14,23 @@
 #define NUM_CHILDREN 4
 #define CHILDREN_NAME "/children_sem"
 
+// define the signatures of the math functions
 double gaussian(double);
 double charge_decay(double);
 
+// declare an array of math functions
 typedef double math_func_t(double);
 static math_func_t* const funcs[NUM_FUNCS] = {&sin, &gaussian, &charge_decay};
 
+// declare counter of child processes
+static volatile sig_atomic_t num_children = 0;
+
+// gaussian function
 double gaussian(double x) {
 	return exp(-(x * x) / 2) / (sqrt(2 * M_PI));
 }
 
+// charge decay function
 double charge_decay(double x) {
 	if (x < 0) 
 		return 0;
@@ -35,31 +42,39 @@ double charge_decay(double x) {
 
 // integrate using the trapezoid method. 
 double integrate_trap(math_func_t* func, double range_start, double range_end, size_t num_steps) {
-	double range_size = range_end - range_start;
-	double dx = range_size / num_steps;
+	double range_size = range_end - range_start; // the size of the integral
+	double dx = range_size / num_steps; // the integral increment amount
 
-	double area = 0;
-	for (size_t i = 0; i < num_steps; i++) {
-		double smallx = range_start + i*dx;
-		double bigx = range_start + (i+1)*dx;
+	double area = 0; // the area of the integral
+	for (size_t i = 0; i < num_steps; i++) { // iterate over all steps in the integral
+		double smallx = range_start + i * dx; // the left-x value
+		double bigx = range_start + (i + 1) * dx; // the right-x value
 
-		area += func(smallx) + func(bigx);
+		area += func(smallx) + func(bigx); // the sum of the function at the x values
 	}
 
-	area = dx * area / 2;
+	area = dx * area / 2; // finish the area calculation
 
-	return area;
+	return area; // return the total area
 }
 
+// get a valid input from stdin
 bool get_valid_input(double* start, double* end, size_t* num_steps, size_t* func_id) {
-	printf("Query: [start] [end] [num_steps] [func_id]\n");
+	printf("Query: [start] [end] [num_steps] [func_id]\n"); // print the query
 
-	size_t num_read = scanf("%lf %lf %zu %zu", start, end, num_steps, func_id);
+	size_t num_read = scanf("%lf %lf %zu %zu", start, end, num_steps, func_id); // read from stdin and grab the values
 
-	return (num_read == 4 && *end >= *start && *num_steps > 0 && *func_id < NUM_FUNCS);
+	return (num_read == 4 && *end >= *start && *num_steps > 0 && *func_id < NUM_FUNCS); // sanity check
 }
 
+// create a named semaphore
 sem_t* init_named_sem(void) {
+    /*
+        create a named semaphore with name "CHILDREN_NAME"
+        , check for the existence of the semaphore
+        , allow read and write permissions
+        and an initial value of "NUM_CHILDREN"
+    */
 	sem_t* children = sem_open(CHILDREN_NAME, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR, NUM_CHILDREN);
 
 	/*
@@ -70,36 +85,59 @@ sem_t* init_named_sem(void) {
 	*/
 	sem_unlink(CHILDREN_NAME);
 
-	return children;
+	return children; // return the semaphore
 }
 
+// spawn a child process to handle the integration
 void spawn_child_process(sem_t* children, double range_start, double range_end, size_t num_steps, size_t func_id) {
-	if (fork())
+	if (fork()) // ensure we are the child process
 		return;
 
-	double area = integrate_trap(funcs[func_id], range_start, range_end, num_steps);
+	double area = integrate_trap(funcs[func_id], range_start, range_end, num_steps); // do the integration
 
+	// print the result
 	printf("The integral of function %zu in range %g to %g is %.10g\n", 
 		func_id, range_start, range_end, area);
 
-	sem_post(children);
-	sem_close(children);
+	sem_post(children); // increment the children semaphore to allow another process to be spawned
+	sem_close(children); // close the child processes access to the semaphore
 
-	exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS); // finished
 }
 
+// asynchronously wait for a child process to exit
+void async_child_wait(int signum) {
+	signal(SIGCHLD, async_child_wait); // re-intantiate the signal
+
+	wait(NULL); // acknowledge/wait for the child's death
+
+	num_children--; // notify main loop that a child has been completely removed
+}
+
+// entry point of the program
 int main(void) {
+    // define variables populated by stdin
 	double range_start, range_end;
 	size_t num_steps, func_id;
 
-	sem_t* children = init_named_sem();
+	sem_t* children = init_named_sem(); // create the named semaphore
 
-	while (!sem_wait(children) && get_valid_input(&range_start, &range_end, &num_steps, &func_id))
-		spawn_child_process(children, range_start, range_end, num_steps, func_id);
+	signal(SIGCHLD, async_child_wait); // create a signal to listen for child process's exiting
 
-	while (wait(NULL) > 0); // wait for all child processes to finish
+    // wait for a child process to be available
+	while (!sem_wait(children)) {
+		while (num_children == NUM_CHILDREN); // a VERY short loop to ENSURE a child process has COMPLETELY exited
 
-	sem_close(children);
+		if (!get_valid_input(&range_start, &range_end, &num_steps, &func_id)) // check for valid input
+			break;
 
-	exit(EXIT_SUCCESS);
+		num_children++;
+		spawn_child_process(children, range_start, range_end, num_steps, func_id); // spawn a child process
+	}
+
+	while (wait(NULL) > 0); // (safety) wait for all child processes to finish (if any)
+
+	sem_close(children); // close parent process's access to the named semaphore, destroying it
+
+	exit(EXIT_SUCCESS); // finished
 }
--- assignment/thread.c
+++ assignment/thread.c
@@ -8,12 +8,15 @@
 #define NUM_FUNCS 3
 #define NUM_THREADS 4
 
+// define the signatures of the math functions
 double gaussian(double);
 double charge_decay(double);
 
+// declare an array of math functions
 typedef double math_func_t(double);
 static math_func_t* const funcs[NUM_FUNCS] = {&sin, &gaussian, &charge_decay};
 
+// create a struct to pass to threads
 typedef struct {
 	double* area;
 	pthread_mutex_t* lock;
@@ -22,10 +25,12 @@ typedef struct {
 	size_t num_steps, func_id;
 } worker_t;
 
+// gaussian function
 double gaussian(double x) {
 	return exp(-(x * x) / 2) / (sqrt(2 * M_PI));
 }
 
+// charge decay function
 double charge_decay(double x) {
 	if (x < 0) 
 		return 0;
@@ -37,46 +42,52 @@ double charge_decay(double x) {
 
 // integrate using the trapezoid method. 
 void* integrate_trap(void* arg) {
-	worker_t* worker = (worker_t*)arg;
+	worker_t* worker = (worker_t*)arg; // grab the worker argument
 
-	double range_size = worker->range_end - worker->range_start;
-	double dx = range_size / worker->num_steps;
+	double range_size = worker->range_end - worker->range_start; // the size of the integral
+	double dx = range_size / worker->num_steps; // the integral increment amount
 
-	math_func_t* func = funcs[worker->func_id];
+	math_func_t* func = funcs[worker->func_id]; // the function to integrate
 
-	double area = 0;
-	for (size_t i = 0; i < worker->num_steps; i++) {
-		double smallx = worker->range_start + i * dx;
-		double bigx = worker->range_start + (i + 1) * dx;
+	double area = 0; // the area of the integral
+	for (size_t i = 0; i < worker->num_steps; i++) { // iterate over all steps in the integral
+		double smallx = worker->range_start + i * dx; // the left-x value
+		double bigx = worker->range_start + (i + 1) * dx; // the right-x value
 
-		area += func(smallx) + func(bigx);
+		area += func(smallx) + func(bigx); // the sum of the function at the x values
 	}
 
-	area = dx * area / 2;
+	area = dx * area / 2; // finish the area calculation
 
-	pthread_mutex_lock(worker->lock);
-	*worker->area += area;
-	pthread_mutex_unlock(worker->lock);
+	pthread_mutex_lock(worker->lock); // acquire lock
+	*worker->area += area; // increment the total area
+	pthread_mutex_unlock(worker->lock); // release lock
 
-	return NULL;
+	return NULL; // finished
 }
 
+// get a valid input from stdin
 bool get_valid_input(double* start, double* end, size_t* num_steps, size_t* func_id) {
-	printf("Query: [start] [end] [num_steps] [func_id]\n");
+	printf("Query: [start] [end] [num_steps] [func_id]\n"); // print the query
 
-	size_t num_read = scanf("%lf %lf %zu %zu", start, end, num_steps, func_id);
+	size_t num_read = scanf("%lf %lf %zu %zu", start, end, num_steps, func_id); // read from stdin and grab the values
 
-	return (num_read == 4 && *end >= *start && *num_steps > 0 && *func_id < NUM_FUNCS);
+	return (num_read == 4 && *end >= *start && *num_steps > 0 && *func_id < NUM_FUNCS); // sanity check
 }
 
+// spawn child threads to perform the integration
 void spawn_child_threads(worker_t workers[], pthread_mutex_t* lock, double range_start, double range_end, size_t num_steps, size_t func_id) {
-	double area = 0;
+	double area = 0; // the total area
 		
-	double dx = (range_end - range_start) / NUM_THREADS;
-	size_t steps_per_thread = num_steps / NUM_THREADS;
+	double dx = (range_end - range_start) / NUM_THREADS; // the "step" size of each sub-integral
+	size_t steps_per_thread = num_steps / NUM_THREADS;  // the number of steps for each sub-integral
+	
+	// if the number of steps is not a multiple of the request
+    if (num_steps % NUM_THREADS != 0) steps_per_thread += 1; // add an extra step (rather have higher accuracy than less)
 
-	pthread_t thread_ids[NUM_THREADS];
+	pthread_t thread_ids[NUM_THREADS]; // store the thread ids
 
+    // create a worker thread
 	for (size_t i = 0; i < NUM_THREADS; i++) {
 		worker_t* worker = &workers[i];
 
@@ -87,26 +98,31 @@ void spawn_child_threads(worker_t workers[], pthread_mutex_t* lock, double range
 		worker->num_steps = steps_per_thread;
 		worker->func_id = func_id;
 
-		pthread_create(&thread_ids[i], NULL, integrate_trap, worker);
+		pthread_create(&thread_ids[i], NULL, integrate_trap, worker); // spawn a thread in the integrate_trap with the worker initiated above
 	}
 
+    // wait for all the threads to finish
 	for (size_t i = 0; i < NUM_THREADS; i++)
 		pthread_join(thread_ids[i], NULL);
 
+    // print the result
 	printf("The integral of function %zu in range %g to %g is %.10g\n", 
 		func_id, range_start, range_end, area);
 }
 
+// entry point of the program
 int main(void) {
+	// define variables populated by stdin
 	double range_start, range_end;
 	size_t num_steps, func_id;
 
-	worker_t workers[NUM_THREADS];
+	worker_t workers[NUM_THREADS]; // define an array of workers (one for each thread)
 	
-	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // create a mutex to protect writing to the total area
 
+	 // spawn the child threads
 	while (get_valid_input(&range_start, &range_end, &num_steps, &func_id))
 		spawn_child_threads(workers, &lock, range_start, range_end, num_steps, func_id);
 
-	exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS); // finished
 }
