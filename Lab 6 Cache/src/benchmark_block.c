1bb4a15d37d9227539eaaf7f955b7d6aabcf54a3
sre90 <sre90@uclive.ac.nz>
2022-09-18 12:50:38 +1200

    argh

--- Lab 6 Cache/src/benchmark_block.c
+++ Lab 6 Cache/src/benchmark_block.c
@@ -8,45 +8,40 @@
 
 #include "matrix.h"
 
-
-void benchmark_blocks(int n, int repeats) {
- 
-  
+void benchmark_blocks(int n, int repeats)
+{
   double *a = random_matrix(n);
-  double *b = random_matrix(n); 
-  
+  double *b = random_matrix(n);
+
   double *res = alloc_matrix(n);
-  
+
   struct timespec start, stop;
   int i;
-  
+
   int blocks = 4;
-  
-  for (blocks = 4; blocks <= 1024; blocks *= 2) {
-    clock_gettime( CLOCK_REALTIME, &start);
-    for(i = 0; i < repeats; ++i) {
+
+  for (blocks = 4; blocks <= n; blocks *= 2)
+  {
+    clock_gettime(CLOCK_REALTIME, &start);
+    for (i = 0; i < repeats; ++i)
+    {
       matrix_mul_blocked(res, a, b, n, blocks);
     }
-    
-    clock_gettime( CLOCK_REALTIME, &stop);
+
+    clock_gettime(CLOCK_REALTIME, &stop);
     double blocked = seconds(start, stop);
-    
 
     printf("%d, blocks: %d, blocked: %.4lf\n", n, blocks, blocked);
   }
-  
+
   free(a);
   free(b);
   free(res);
 }
 
-
-
-int main(int argc, char**argv) {
-  
-
-
-  benchmark_blocks(1024, 1);
+int main(int argc, char **argv)
+{
+  benchmark_blocks(2048, 1);
 
   return 0;
 }
